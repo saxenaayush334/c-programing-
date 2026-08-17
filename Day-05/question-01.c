7b70324187551627bc14a34b98ecb352d9dcd134
saxenaayush334 <saxenaayush334@gmail.com>
2026-08-17 12:38:35 +0530

    Implement simple and compound interest calculation

---
+++ Day-05/question-01.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float principal, rate, time;
+    float simpleInterest, compoundInterest, amount;
+
+    printf("Enter principal amount: ");
+    scanf("%f", &principal);
+
+    printf("Enter rate of interest: ");
+    scanf("%f", &rate);
+
+    printf("Enter time in years: ");
+    scanf("%f", &time);
+
+    // Simple Interest
+    simpleInterest = (principal * rate * time) / 100;
+
+    // Compound Interest
+    amount = principal * pow((1 + rate / 100), time);
+    compoundInterest = amount - principal;
+
+    printf("Simple Interest = %.2f\n", simpleInterest);
+    printf("Compound Interest = %.2f\n", compoundInterest);
+
+    return 0;
+}
